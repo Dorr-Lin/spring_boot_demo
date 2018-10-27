5ace135ce5f6bd14cf9e49a0cad90ef80d0980b9
dorrtest <dorrgle@gmail.com>
2018-10-27 10:55:17 +0800

    init commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello home 123456!";
+    }
+
+    @RequestMapping("/test")
+    public String testFunction() {
+        return "test function";
+    }
+}
